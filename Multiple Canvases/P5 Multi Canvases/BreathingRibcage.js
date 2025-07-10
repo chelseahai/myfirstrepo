17313393ab1b0955649a7252f654249f46e7f2b8
chelseahai <jh5138@columbia.edu>
2025-07-09 20:14:11 -0400

    Update2

--- Multiple Canvases/P5 Multi Canvases/BreathingRibcage.js
+++ Multiple Canvases/P5 Multi Canvases/BreathingRibcage.js
@@ -1,26 +1,26 @@
-let t = 0;
-
-function setup() {
-  createCanvas(600, 400);
-  stroke(50);
-  noFill();
-}
-
-function draw() {
-  background(255);
-  translate(width / 2, height / 2);
-
-  let breath = sin(t) * 20;
-  for (let i = 0; i < 10; i++) {
-    let y = i * 10;
-    let offset = map(i, 0, 9, 60, 100) + breath;
-    beginShape();
-    curveVertex(-offset, y);
-    curveVertex(-offset, y);
-    curveVertex(0, y + 5);
-    curveVertex(offset, y);
-    curveVertex(offset, y);
-    endShape();
-  }
-  t += 0.03;
-}
+let BreathingRibcage = function(p) {
+  let t = 0;
+  p.setup = function() {
+    p.createCanvas(600, 400);
+    p.stroke(50);
+    p.noFill();
+  };
+  p.draw = function() {
+    p.background(255);
+    p.translate(p.width / 2, p.height / 2);
+    let breath = p.sin(t) * 20;
+    for (let i = 0; i < 10; i++) {
+      let y = i * 10;
+      let offset = p.map(i, 0, 9, 60, 100) + breath;
+      p.beginShape();
+      p.curveVertex(-offset, y);
+      p.curveVertex(-offset, y);
+      p.curveVertex(0, y + 5);
+      p.curveVertex(offset, y);
+      p.curveVertex(offset, y);
+      p.endShape();
+    }
+    t += 0.03;
+  };
+};
+new p5(BreathingRibcage, 'canvas1');
